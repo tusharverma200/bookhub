4350a79d8f9dfdeff4ca736232d3e124a3e3d694
Tushar Verma <vermatushar200@gmail.com>
2025-04-25 01:34:54 +0530

    proxy added

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/book': 'https://bookhub-ycew.vercel.app', // Proxy API requests
+    },
+  },
 })
